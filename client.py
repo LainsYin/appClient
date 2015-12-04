a482b4ef6b938ab3b64e5533cd025e10056c0174
LainsYin <yinjindu2011@126.com>
2015-12-04 11:53:39 +0800

    初步可用

--- client.py
+++ client.py
@@ -8,6 +8,7 @@ import signal
 import json
 import struct
 import logging
+import ConfigParser
 import threading
 from struct import pack, unpack
 
@@ -38,13 +39,17 @@ def register_options():
                       default="192.168.1.156", help="specify host, default is 192.168.1.156")
     parser.add_option("-p", "--port", dest="port",
                       type="int",
-                      default=3050, help="specify port, default is 58849")
+                      default=3050, help="specify port, default is 3050")
+    parser.add_option("-f", "--fun", dest="fun",
+                      type="int",
+                      default=90001, help="specify function num, default is 90001")
     parser.add_option("-n", "--num", dest="num",
                       type="int",
                       default=1, help="specify threads num, default is 1")
+
     parser.add_option("-d", "--daemon", dest="daemon",
                       action='store_true',
-                      default=False, help="set daemon process, default is false")
+                      default=True, help="set daemon process, default is true")
 
     (options, args) = parser.parse_args()
     return options
@@ -79,19 +84,8 @@ def verify_data(data):
         logging.error('received data body is not JSON string')
         return ''
 
-    logging.debug('header:%s body:%s' % (header, json.dumps(body, indent=4)))
-
-    try:
-        if parts[2] is 90011:
-            logging.info('receive data %d: %s' % parts[2], body)
-
-        if parts[2] is 90012:
-            logging.info('receive data %d: %d' % parts[2], body)
-
-        if parts[2] is 90013:
-            logging.info('receive data %d: %d' % parts[2], body)
-    except:
-        logging.error('data in body is not JSON string')
+    logging.debug('received data :  header:%s ' % header)
+    logging.debug('received data :  body:%s' % (json.dumps(body, indent=4)))
 
 
 class Client(threading.Thread):
@@ -114,24 +108,17 @@ class Client(threading.Thread):
             return
 
         self.rec_data()
-        # self.send()
 
-    def send(self, to_uid):
+    def send(self, header, body):
 
-        # body = {"touid": to_uid}
-        # json_arr = json.dumps(body)
-        # orig = [17, 100, 90003, 65536, len(json_arr), 520]
-
-        body = {"touid": to_uid, "message": "hello world", "type": 1}
-        json_arr = json.dumps(body)
-        orig = [17, 100, 90001, 65536, len(json_arr), 520]
+        orig = header
+        assert isinstance(orig, object)
+        orig[4] = len(body)
 
         elems = [socket.htonl(x) for x in orig]
-        header = pack('6I', elems[0], elems[1], elems[2],
+        header_net = pack('6I', elems[0], elems[1], elems[2],
                       elems[3], elems[4], elems[5])
-        msg = header + json_arr
-        # msg = header
-        print json_arr, header
+        msg = header_net + body
         try:
             self._sock.send(msg)
         except socket.error, arg:
@@ -151,15 +138,13 @@ class Client(threading.Thread):
 
     def rec_data(self):
         self._sock.setblocking(0)
-        recv_buf = ''
         while True:
             time.sleep(0.1)
             try:
-                recv_buf = self._sock.recv(4096)
-                if len(recv_buf) >= 24:
-                    print len(recv_buf)
-                    verify_data(recv_buf)
-            except Exception, e:
+                rec_buf = self._sock.recv(4096)
+                if len(rec_buf) >= 24:
+                    verify_data(rec_buf)
+            except :
                 pass
                 # logging.error('recv data error %s' % e)
 
@@ -168,15 +153,21 @@ if __name__ == '__main__':
 
     init_log()
     opts = register_options()
-
     logging.info('start %d threads to server %s:%d ...' % (opts.num, opts.host, opts.port))
+
+    conf = ConfigParser.ConfigParser()
+    conf.read("./config.ini")
+    fun_num = opts.fun
+    header_str = conf.get("%s" % fun_num, "header").split(",")
+    header = [int(x) for x in header_str]
+    body = conf.get("%s" % fun_num, "body")
 
     for i in xrange(opts.num):
         client = Client(opts.host, opts.port)
         THREADS.append(client)
 
     for i in THREADS:
-        # i.setDaemon(opts.daemon)
+        i.setDaemon(opts.daemon)
         i.start()
 
     """ register control+c kill thread
@@ -184,19 +175,13 @@ if __name__ == '__main__':
     signal.signal(signal.SIGTERM, sig_handler)
     signal.signal(signal.SIGINT, sig_handler)
 
-    # while True:
-    input_text = raw_input("Input Fun Num >>>> ")
-        # if input_text.isdigit():
-    THREADS[0].send("60HNr67d")
-        # elif "quit" == input_text.lower() or "exit" == input_text.lower():
-        #     exit(0)
+    logging.info("Waiting for 1 second")
+    time.sleep(1)
+    THREADS[0].send(header, body)
 
-    # master thread to catch signal
+# master thread to catch signal
     while not STOP:
         time.sleep(0.01)
-
-    # for i in range(len(THREADS)):
-    #     THREADS[i].join()
 
     logging.info('stop ...')
 
